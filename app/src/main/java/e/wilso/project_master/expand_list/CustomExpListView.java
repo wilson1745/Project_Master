8819007369e4716982ae1c05b7c7155633a70adb
wilson1745 <wilson-power0130@hotmail.com>
2018-07-24 17:54:00 +0100

    finish expandablelistview (need to arrange it)

---
+++ app/src/main/java/e/wilso/project_master/expand_list/CustomExpListView.java
@@ -0,0 +1,18 @@
+package e.wilso.project_master.expand_list;
+
+import android.content.Context;
+import android.widget.ExpandableListView;
+
+public class CustomExpListView extends ExpandableListView {
+
+   public CustomExpListView(Context context) {
+        super(context);
+   }
+
+   protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+      widthMeasureSpec = MeasureSpec.makeMeasureSpec(960, MeasureSpec.AT_MOST);
+      heightMeasureSpec = MeasureSpec.makeMeasureSpec(20000, MeasureSpec.AT_MOST);
+      super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+   }
+}
+
