6b7f46f3b2127c1dc11028eb922e480f062e172c
wilson1745 <wilson-power0130@hotmail.com>
2018-07-25 23:24:04 +0100

    makeMeasureSpec fixed

--- app/src/main/java/e/wilso/project_master/expand_list/CustomExpListView.java
+++ app/src/main/java/e/wilso/project_master/expand_list/CustomExpListView.java
@@ -1,6 +1,7 @@
 package e.wilso.project_master.expand_list;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.ExpandableListView;
 
 public class CustomExpListView extends ExpandableListView {
@@ -10,8 +11,9 @@ public class CustomExpListView extends ExpandableListView {
    }
 
    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+      //Log.e("TAG", "onMeasure is working: " + widthMeasureSpec + ", " + heightMeasureSpec);
       // 螢幕寬度的關鍵
-      widthMeasureSpec = MeasureSpec.makeMeasureSpec(1400, MeasureSpec.EXACTLY);
+      //widthMeasureSpec = MeasureSpec.makeMeasureSpec(960, MeasureSpec.EXACTLY);
       //widthMeasureSpec = MeasureSpec.makeMeasureSpec(960, MeasureSpec.AT_MOST);
       heightMeasureSpec = MeasureSpec.makeMeasureSpec(20000, MeasureSpec.AT_MOST);
       super.onMeasure(widthMeasureSpec, heightMeasureSpec);
